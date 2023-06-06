3490bcc16716e032832d6639a347a7b857c1a464
Teguh Pratama <teguhpratama337@gmail.com>
2023-06-06 13:01:20 +0900

    change database setting 3

--- lib/db.ts
+++ lib/db.ts
@@ -1,38 +1,38 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI;
+// const MONGO_URI = process.env.MONGO_URI;
 
-interface ConnectionState {
-  isConnected?: number;
-}
+// interface ConnectionState {
+//   isConnected?: number;
+// }
 
-const connectionState: ConnectionState = {};
+// const connectionState: ConnectionState = {};
 
-export async function mongooseConnect(): Promise<void> {
-  // if (connectionState.isConnected) {
-  //   console.log('Already connected');
-  //   return;
-  // }
+// export async function mongooseConnect(): Promise<void> {
+//   // if (connectionState.isConnected) {
+//   //   console.log('Already connected');
+//   //   return;
+//   // }
 
-  try {
-    if (mongoose.connections.length > 0) {
-      connectionState.isConnected = mongoose.connections[0].readyState;
+//   try {
+//     if (mongoose.connections.length > 0) {
+//       connectionState.isConnected = mongoose.connections[0].readyState;
 
-      if (connectionState.isConnected === 1) {
-        console.log('Use previous connection');
-        return;
-      }
+//       if (connectionState.isConnected === 1) {
+//         console.log('Use previous connection');
+//         return;
+//       }
 
-      await mongoose.disconnect();
-    }
+//       await mongoose.disconnect();
+//     }
 
-    const db = await mongoose.connect(MONGO_URI!);
-    console.log('New connection');
-    connectionState.isConnected = db.connections[0].readyState;
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
+//     const db = await mongoose.connect(MONGO_URI!);
+//     console.log('New connection');
+//     connectionState.isConnected = db.connections[0].readyState;
+//   } catch (error) {
+//     return Promise.reject(error);
+//   }
+// }
 
 // async function disconnect(): Promise<void> {
 //   if (connectionState.isConnected) {
@@ -47,13 +47,15 @@ export async function mongooseConnect(): Promise<void> {
 
 // const db = { connect, disconnect };
 
-// import mongoose from 'mongoose';
-
-// export function mongooseConnect() {
-//   if (mongoose.connection.readyState === 1) {
-//     return mongoose.connection.asPromise();
-//   } else {
-//     const uri = process.env.MONGO_URI;
-//     return mongoose.connect(uri!);
-//   }
-// }
+export function mongooseConnect() {
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection.asPromise();
+  } else {
+    try {
+      const uri = process.env.MONGO_URI;
+      return mongoose.connect(uri!);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+}
